05c779aa4d17d3319cb0ba4c8d01e011be92107a
Don Blair <donblair@gmail.com>
2025-03-21 23:19:01 -0400

    update

---
+++ double_uart_itbm4_mesh_send_depth_battery.py
@@ -0,0 +1,53 @@
+import board
+import busio
+import digitalio
+import time
+from analogio import AnalogIn
+
+SLEEP_INTERVAL = 10
+
+#depth_trigger = digitalio.DigitalInOut(board.A1)
+#depth_trigger.direction = digitalio.Direction.OUTPUT
+#depth_trigger.value=False
+
+analog_in = AnalogIn(board.A4)
+
+uart_sense = busio.UART(board.A2, board.A3, baudrate=9600)
+
+uart_mesh = busio.UART(board.TX, board.RX, baudrate=115200, timeout=0)
+
+def get_depth_crude():
+
+    depth=-1
+    
+    data = uart_sense.read(32)  # read up to 32 bytes
+
+    if data is not None:
+       
+        data_string = ''.join([chr(b) for b in data])
+        #print("%r" % data_string)
+        items = data_string.split('\r')
+        for s in items:
+            #print(s,len(s))
+            if len(s)==5:
+                depth=int(s[1:])
+    return(depth)
+
+def get_battery_voltage(pin):
+    return (pin.value * 3.3) / 65536 * 2 # even voltage divider
+    
+while True:
+
+    depth=get_depth_crude()
+    battery = get_battery_voltage(analog_in)
+    
+    print("depth=",depth)
+   
+    sendstring = str(depth)+","+str(battery)
+
+    print("Sending "+sendstring)
+    
+    uart_mesh.write(bytes(sendstring, "ascii"))
+    
+    time.sleep(SLEEP_INTERVAL)
+
--- poster.py
+++ poster.py
@@ -10,24 +10,31 @@ import wifi
 
 import adafruit_requests
 
+import board
+import busio
+import digitalio
+import time
+
+uart_mesh = busio.UART(board.TX, board.RX, baudrate=115200, timeout=0)
+
 # Get WiFi details, ensure these are setup in settings.toml
 ssid = os.getenv("CIRCUITPY_WIFI_SSID")
 password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
+public_key=os.getenv("BAYOU_PUBLIC_KEY")
+private_key=os.getenv("BAYOU_PRIVATE_KEY")
 
-public_key = "xg4k7vgvpzrs"
-private_key = "----"
 
-distance_meters=3.
-
-# these will stay fixed:
+#public_key = "7pb4kprfp5cn"
+#private_key = "ismxbyq54vbd"
 base_url = "http://bayou.pvos.org/data/"
 full_url = base_url+public_key
 
+distance_meters=1.
+
 TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
 JSON_GET_URL = "https://httpbin.org/get"
-JSON_POST_URL = "https://httpbin.org/post"
+JSON_POST_URL = base_url+public_key
 
-JSON_POST_URL = full_url
 
 # Initalize Wifi, Socket Pool, Request Session
 pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
@@ -35,42 +42,45 @@ ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl_context)
 rssi = wifi.radio.ap_info.rssi
 
-print(f"\nConnecting to {ssid}...")
-print(f"Signal Strength: {rssi}")
-try:
-    # Connect to the Wi-Fi network
-    wifi.radio.connect(ssid, password)
-except OSError as e:
-    print(f"❌ OSError: {e}")
-print("✅ Wifi!")
 
-print(f" | GET Text Test: {TEXT_URL}")
-with requests.get(TEXT_URL) as response:
-    print(f" | ✅ GET Response: {response.text}")
-print("-" * 80)
+while True:
 
-print(f" | GET Full Response Test: {JSON_GET_URL}")
-with requests.get(JSON_GET_URL) as response:
-    print(f" | ✅ Unparsed Full JSON Response: {response.json()}")
-print("-" * 80)
+    data = uart_mesh.read(32)
+    
+    if data is not None:
+       
+        data_string = ''.join([chr(b) for b in data])
+        #print("%r" % data_string)
+        print(data_string)
+        items = data_string.split(":")
+        print(items)
+        if len(items)==2:
+            sensed=items[1].strip()
+            print("sensed=",sensed)
+            senseparse=sensed.split(",")
+            depth=senseparse[0]
+            battery=senseparse[1]
+            
+            print("depth=",depth)
+            print("battery=",battery)
+            
+            print(f"\nConnecting to {ssid}...")
+            print(f"Signal Strength: {rssi}")
+            try:
+                # Connect to the Wi-Fi network
+                wifi.radio.connect(ssid, password)
+            except OSError as e:
+                print(f"❌ OSError: {e}")
+            print("✅ Wifi!")
 
-DATA = "This is an example of a JSON value"
-print(f" | ✅ JSON 'value' POST Test: {JSON_POST_URL} {DATA}")
-with requests.post(JSON_POST_URL, data=DATA) as response:
-    json_resp = response.json()
-    # Parse out the 'data' key from json_resp dict.
-    print(f" | ✅ JSON 'value' Response: {json_resp['data']}")
-print("-" * 80)
 
-#json_data = {"Date": "January 1, 1970"}
+            json_data = {"private_key":private_key, "node_id":0,"distance_meters":depth,"battery_volts":battery}
 
-json_data = {"private_key":private_key, "node_id":0,"distance_meters":distance_meters}
-print(f" | ✅ JSON 'key':'value' POST Test: {JSON_POST_URL} {json_data}")
-with requests.post(JSON_POST_URL, json=json_data) as response:
-    json_resp = response.json()
-    # Parse out the 'json' key from json_resp dict.
-    print(f" | ✅ JSON 'key':'value' Response: {json_resp['json']}")
-print("-" * 80)
 
-print("Finished!")
+            with requests.post(JSON_POST_URL, json=json_data) as response:
+                resp = response.text
+                print(resp)
+            
+            
+                
 
